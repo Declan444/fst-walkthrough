742ec59a16f508d683875008d69b793474f98449
Declan <laresearchlabs@gmail.com>
2024-06-17 20:35:56 +0100

    Setup heroku and link elephantSQL database

--- env.py
+++ env.py
@@ -1,1 +1,4 @@
 import os
+
+os.environ["DATABASE_URL"]="postgres://jidqzrxr:YoUuDrPnJF2lMJhW5jtWOsHmpSYcG-oV@manny.db.elephantsql.com/jidqzrxr"
+os.environ["SECRET_KEY"]="2tfth7j1)3b(tqrh@i-^hjmx&fcjcpn(6=3v6ywtu=paorf0w&"
