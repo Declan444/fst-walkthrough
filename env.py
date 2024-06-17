65efc3f5dadb431fb5bebb3bcdf3236501927f9d
Declan <laresearchlabs@gmail.com>
2024-06-17 21:05:18 +0100

    deployment

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"]="postgres://jidqzrxr:YoUuDrPnJF2lMJhW5jtWOsHmpSYcG-oV@manny.db.elephantsql.com/jidqzrxr"
 os.environ["SECRET_KEY"]="2tfth7j1)3b(tqrh@i-^hjmx&fcjcpn(6=3v6ywtu=paorf0w&"
+os.environ["CLOUDINARY_URL"]= "cloudinary://<your_api_key>:<your_api_secret>@donkem6h4"
